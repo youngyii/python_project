876c8795e24d14e138d1a3226c891f028338c8a9
hyunnnnj <124047728+hyunnnnj@users.noreply.github.com>
2023-11-30 16:53:07 +0900

    Add files via upload

--- mypage.py
+++ mypage.py
@@ -16,16 +16,19 @@ def PwChange():
         else:
             print("비밀번호가 틀렸습니다. 다시 시도해 주세요.")
 
+def LogOut():
+    #저장된 user 정보 초기화 하기
+    print("로그아웃 완료.")
+    
 # 비밀번호 변경
-pw = "assa"
+pw = "assa" #>> 합친 후에 저장된 비번 불러오는 걸로 바꾸기
 
 choice = input("비밀번호 변경 (1 입력)\n로그아웃 (2 입력) : ")
 
 if choice == "1":
     PwChange()
 elif choice == "2":
-    #현재 id, pw 정보 초기화
-    print("로그아웃 되었습니다.")
+    LogOut()
 else:
     print("잘못된 입력입니다.")
 
